33e1020d4e0f4111d02e333b710e26fbb67bb86f
Dolores Portalatin <admin@doloresportalatin.info>
2013-06-05 21:06:13 -0400

    Updated the blog submission script to save contact information

--- submit/save.php
+++ submit/save.php
@@ -1,1 +1,40 @@
-Thank you for submitting a post to Arch Women!
+<?php
+// Text Feilds that correspond to the html form
+$author    = @$_POST['author']
+$email     = @$_POST['email']
+$title     = @$_POST['title'];
+$date      = @$_POST['date'];
+$excerpt   = @$_POST['excerpt'];
+$tags      = @$_POST['tags'];
+$comment   = @$_POST['comment'];
+
+// File and Path variables
+
+$path = "/srv/http/archwomen.org/test/submit/drafts/";
+$filename = "draft.md";
+
+// Merge all the variables with text in a single variable.
+$f_data= '
+author: '.$author.'
+email: '.$email.'
+title: '.$title.'
+date: '.$date.'
+excerpt: '.$excerpt.'
+tags: '.$tags.'
+
+'.$comment.'
+';
+
+while (file_exists($path.$filename))
+    {
+        $fnum++;
+        $filename = "draft".$fnum.".md";
+    }
+$handle = fopen($path.$filename, "w") or die("Couldn't open 
+$filename for writing!");
+fwrite($handle,$f_data) or die("Couldn't write values to 
+file!");
+fclose($handle);
+
+echo 'Thank you for submitting a post to Arch Women!'
+?>
